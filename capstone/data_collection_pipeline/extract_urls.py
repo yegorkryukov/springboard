67123bd3412d7e181c389921edd645bdb4a221b5
Yegor Kryukov <yegor.kryukov@gmail.com>
2020-07-03 14:59:06 -0400

    Added stored URLs count

--- capstone/data_collection_pipeline/extract_urls.py
+++ capstone/data_collection_pipeline/extract_urls.py
@@ -41,13 +41,70 @@ def get_urls_finviz(ticker):
         return urls
     logging.info(f'No URLs found at finviz.com for {ticker}')
     return False
-            
+
+def get_urls_yahoo(ticker):
+    """Obtain latest news for `ticker` from finance.yahoo.com
+
+    Parameters
+    ----------
+    ticker :  str, ticker to collect news for
+
+    Output
+    ------
+    urls   : list, list of urls
+    """
+    from selenium import webdriver
+    import time
+    from random import randint
+    from selenium.webdriver.firefox.options import Options
+
+    js = """var scrollingElement = (document.scrollingElement || document.body);
+                    scrollingElement.scrollTop = scrollingElement.scrollHeight;"""
+
+    url = f'https://finance.yahoo.com/quote/{ticker}'
+    urls_list = set()
+    
+    try: 
+        options = Options()
+        options.add_argument("--headless")
+        browser = webdriver.Firefox(options=options, executable_path=r'geckodriver')
+        browser.get(url)
+        logging.info(f'Headless Firefox Initialized for URL: {url}')
+        if browser.current_url == f'https://finance.yahoo.com/lookup?s={ticker}':
+            logging.info(f'Extracting URLs failed. Ticker {ticker} does not exist')
+            return False
+        browser.execute_script(js)
+        time.sleep(randint(3,10))
+        items_list = browser.find_elements_by_xpath('//h3/a')
+        num_of_items = 0
+        while num_of_items != len(items_list):
+            num_of_items = len(items_list)
+            browser.execute_script(js)
+            time.sleep(randint(3,5))
+            items_list = browser.find_elements_by_xpath('//h3/a')
+            logging.info(f'{ticker} found {len(items_list)} urls so far')
+
+        for item in items_list:
+            urls_list.add(item.get_attribute('href'))
+
+        logging.info(f'Found {len(urls_list)} urls for {ticker}')
+    except Exception as e:
+        logging.info(f'Extracting URLs failed. Error:\n{e}')
+
+    browser.quit()
+    if urls_list:
+        return list(urls_list)
+    logging.info(f'No URLs found at finance.yahoo.com for {ticker}')
+    return False
+
 def save_urls_to_db(urls, ticker=''):
     """Saves urls to MongoDB checking for duplicates
     """
     import pymongo as pm
     import pandas as pd
     import logging
+
+    logging.info(f'Got {len(urls)} URLs to save for ticker {ticker}')
 
     client = pm.MongoClient('mongodb://localhost:27017')
 
@@ -61,6 +118,18 @@ def save_urls_to_db(urls, ticker=''):
             {'ticker':ticker},
             {'news.url':1, '_id':0}
         )
+
+        qty_stored_to_process_before = c.find_one(
+            {'ticker':ticker},
+            {'urls_to_process':1, '_id':0}
+        )
+
+        if len(qty_stored_to_process_before) > 0:
+            qty_stored_to_process_before = len([
+                _ for _ 
+                in qty_stored_to_process_before['urls_to_process']
+            ])
+        else: qty_stored_to_process_before = 0
         
         if len(processed_urls) > 0:
             processed_urls = [
@@ -88,7 +157,9 @@ def save_urls_to_db(urls, ticker=''):
             upsert = True
         )
 
-        logging.info(f'Saved <= {len(to_process_urls)} new urls for {ticker}')
+
+
+        logging.info(f'Saved {len(to_process_urls) - qty_stored_to_process_before} new urls for {ticker}')
     # else:
         # collection = client['news']['not_processed']
         # check for duplicates
@@ -109,13 +180,18 @@ if __name__ == '__main__':
         logger.addHandler(handler)
 
     logger.setLevel(logging.INFO)
-    logging.info(f'Starting url extraction at {datetime.datetime.now()}')
+    logging.info(f'Starting URL extraction')
 
-    ticker = 'A'
-    urls = get_urls_finviz(ticker)
-    if urls: 
-        save_urls_to_db(urls, ticker=ticker)
-    
+    tickers = ['A']
+    for ticker in tickers:
+        urls_finviz = get_urls_finviz(ticker)
+        print(urls_finviz)
+        if urls_finviz: save_urls_to_db(urls_finviz, ticker=ticker)
+
+        # urls_yahoo = get_urls_yahoo(ticker)
+        # if urls_yahoo: save_urls_to_db(urls_yahoo, ticker=ticker)
+
+
     logging.info('Extraction finished')
 
 
