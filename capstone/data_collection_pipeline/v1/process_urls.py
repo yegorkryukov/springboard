b80e96b5c02532a3a23eafe1846fcbc11a39d1a0
Yegor Kryukov <yegor.kryukov@gmail.com>
2020-07-06 01:39:13 -0400

    Working version

--- capstone/data_collection_pipeline/v1/process_urls.py
+++ capstone/data_collection_pipeline/v1/process_urls.py
@@ -1,113 +1,67 @@
-def delete_url_doc(url):
-    """Deletes a mongodb doc with `url` field
+def detect_and_move(ticker):
+    """Looks up for a document in one collection
+    and moves to another one if finds direct mentions
+    of `ticker` in the url's article text. Downloads article
+    text if needed
     """
-    collection.delete_one({'url':url})
-    logging.info(f'Deleted from DB URL {url}')
+    from bson.objectid import ObjectId
+    import pymongo as pm
+    import logging
+    import re
 
-def scrape(url):
-    """
-    Scrapes an article from the 'url' 
-    
-    Parameters:
-    --------
-    url         : str, url to scrape
-    
-    Returns:
-    --------
-    Article's html and features stored to db, 
-    
-    """
-    logging.info(f"Processing {url}")
-    # print(f"Processing {url}")
+    client = pm.MongoClient('mongodb://localhost:27017')
+    source_collection = client['news']['not_processed']
+    target_collection = client['news']['recommendations']
 
-    g = Goose()
-    
-    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:79.0) Gecko/20100101 Firefox/79.0"}
-    cookies = {"cookie":"prov=f155b877-1d05-46c0-e4c8-06710700ab38; _ga=GA1.2.517998572.1588979467; __gads=ID=dff7ccb26af6d704:T=1588979467:S=ALNI_MYjeHrNVGWFTWjnWwl91eovS3W_MQ; __qca=P0-1509564436-1588979467447; _gid=GA1.2.1286355178.1593617896; _gat=1"}
+    source_doc = source_collection.find_one(
+        {'$text': {'$search': ticker, '$caseSensitive': True}})
 
-    requests.adapters.DEFAULT_RETRIES = 1
-    
-    try:
-        url_get = requests.get(url, cookies=cookies, headers=headers, timeout=3)
-        if url_get.status_code == 200:
-            article = g.extract(raw_html=url_get.text)
-            
-            collection.update_one(
-            {'url' : url},
-            {'$set':
-                    {
-                        'date'     : article.publish_date,
-                        'title'    : article.title,
-                        'text'     : article.cleaned_text,
-                        'keywords' : article.meta_keywords,
-                        'summary'  : article.meta_description
-                    }
+    if source_doc and ('text' in source_doc.keys()):
+        logging.info(f"{ticker}: found document. _id: {source_doc['_id']}")
+
+        target_collection.update_one(
+            {'ticker' : ticker},
+            {'$addToSet':
+             {'news':
+              {
+                'url'      : source_doc['url'],
+                'date'     : source_doc['date'],
+                'title'    : source_doc['title'],
+                'text'     : " ".join(re.split(r'[\n\t]+', source_doc['text'])),
+                'keywords' : source_doc['keywords'],
+                'summary'  : source_doc['summary']
+              }
+             }      
             },
                 upsert=True
             )
-            logging.info(f'Saved {url} to DB')
-            return True
-        else:
-            logging.info(f'Did not parse. Error: requests status code {url_get.status_code}')
-            delete_url_doc(url)
-            return False
-    except Exception as e:
-        logging.info(f'Data not saved. Error: {e}')
-        delete_url_doc(url)
+        logging.info(f"Added article to {target_collection.name}. URL: {source_doc['url']}")
+        source_collection.delete_one({'_id': ObjectId(source_doc['_id'])})
+        logging.info(f"Deleted article {ObjectId(source_doc['_id'])} from {source_collection.name}")
+        return True
+    else:
+        logging.info(f"{ticker}: no docs found in {source_collection.name}")
         return False
 
-import pymongo as pm
-import pandas as pd
-import logging
-from multiprocessing import Pool
-import datetime
-from goose3 import Goose
-import requests
-
-
-# Start MongoDB
-# !brew services start mongodb-community@4.2
-
-# Stop MongoDB
-# !brew services stop mongodb-community@4.2
-
 if __name__ == '__main__': 
+    import logging
+    from yahoo_fin import stock_info as si 
+
     logger = logging.getLogger()
-    handler = logging.FileHandler('logs/content_extraction.log')
+    handler = logging.FileHandler('logs/process_url.log')
     formatter = logging.Formatter(
             '%(asctime)s| %(levelname)s| %(message)s')
     handler.setFormatter(formatter)
     if not len(logger.handlers): 
         logger.addHandler(handler)
+
     logger.setLevel(logging.INFO)
-    
-    logging.info(f'Starting url extraction at {datetime.datetime.now()}')
-    
-    client = pm.MongoClient('mongodb://localhost:27017')
-    collection = client['news']['not_processed']
-    
-    saved_urls = [
-        rec['url'] for rec in collection.find({}, {'url':1, 'qty':1})
-    ]
-    logging.info(f'URL in DB: {len(saved_urls)}')
-    
-    to_process_urls = list(pd.read_csv('media/usanews_2015_2020.csv',index_col=0).url)
-    logging.info(f'URL in CSV: {len(to_process_urls)}')
+    logging.info(f'Starting URL processing')
 
-    for url in to_process_urls:
-        if url not in saved_urls:
-            scrape(url)
-            to_process_urls.remove(url)
-        else:
-            to_process_urls.remove(url)
-    
-    pd.DataFrame({'url':to_process_urls}).to_csv('media/usanews_2015_2020.csv')
+    # FIX LOGIC FOR ADS TICKER OR 
+    for ticker in si.tickers_sp500():
+        moving = True
+        while moving:
+            moving = detect_and_move(ticker)
 
-    # count urls in db again
-    saved_urls = [
-        rec['url'] for rec in collection.find({}, {'url':1, 'qty':1})
-    ]
-
-    logging.info(f'Finished at {datetime.datetime.now()}')
-    logging.info(f'URL in DB: {len(saved_urls)}')
-    logging.info(f'URL in CSV: {len(to_process_urls)}')
+    logging.info(f'URL processing ended')
